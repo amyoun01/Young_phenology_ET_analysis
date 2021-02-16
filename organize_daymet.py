36f27b838cc709548aada401d1e9550cdb6fdbdf
amyoung <amyoun01@gmail.com>
2021-02-16 14:58:21 -0700

    working on reading and reprojecting daymet maps

--- organize_daymet.py
+++ organize_daymet.py
@@ -19,21 +19,7 @@ import numpy as np
 import pandas as pd
 
 import gdal
-#import rasterio
-#import rasterio.plot
 import geopandas as gpd
-
-#import matplotlib as mpl
-#import matplotlib.pyplot as plt
-#import matplotlib.patches as mpatches
-#import matplotlib.patheffects as pe
-#from matplotlib.legend_handler import HandlerPatch
-#from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-#from matplotlib import cm
-#from matplotlib import gridspec
-
-#from adjustText import adjust_text
-
 
 wdir = "/Volumes/GoogleDrive/My Drive/Young_phenology_ET_analysis"
 wdir_shell_str = "/Volumes/GoogleDrive/My\ Drive/Young_phenology_ET_analysis"
@@ -47,20 +33,25 @@ site_info = pd.read_csv('pheno_flux_sites_to_use.csv')
 site_info_geo = gpd.GeoDataFrame(site_info, 
                                  geometry=gpd.points_from_xy(site_info.lon, site_info.lat))
 
-# First, make gridded maps of aridity index (AI). PET will be calculated using
-# Thornwaite's equation (1948) 
+# First, make gridded maps of aridity index (AI = PET/P). PET will be calculated using
+# Thornthwaite's equation (1948)
 
-for v in climvars:
-    
-    os.chdir(wdir + climdata_dir + "/" + climvars[v][0:4])
-    
-    for y in yrs:
+for y in yrs:
         
-        fn_i = "daymet_v4_" + climvars[v] + "_na_" + str(yrs[y]) + ".tif"
+    os.chdir(wdir + climdata_dir + "/prcp")
+        
+        fn_i = "daymet_v4_" + climvars[v] + "_na_" + str(y) + ".tif"
         rst_i = gdal.Open(fn_i,gdal.GA_ReadOnly)
         
-        rst_arr_i = rst_i.ReadAsArray()
-
-        # This is a new line
+        projinfo = rst_i.GetProjection()
+        geoTransform = rst_i.GetGeoTransform()
         
+        rst_arr_i = rst_i.ReadAsArray()
+        rst_arr_i[rst_arr_i == -9999] = np.nan
+                
+        for m in range(0,12):
+            
+            rst_arr_im = rst_arr_i[m,:,:]
+
+                
             
